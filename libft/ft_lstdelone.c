8eb163f4f9c30f58c381912e962eb6290f28be9e
Axel Baudrillart <axbaudri@bess-f4r3s11.clusters.42paris.fr>
2025-02-13 17:24:10 +0100

    minishell

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,24 +6,11 @@
 /*   By: axbaudri <axbaudri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 19:26:57 by axbaudri          #+#    #+#             */
-/*   Updated: 2025/02/11 18:18:23 by axbaudri         ###   ########.fr       */
+/*   Updated: 2025/02/13 17:24:04 by axbaudri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*void	ft_lstdelone(t_list *lst, void (*del)(void *))
-{
-	t_list	*temp;
-
-	if (lst)
-	{
-		temp = lst->next;
-		del(lst->content);
-		free(lst);
-		lst = temp;
-	}
-}*/
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
@@ -32,3 +19,15 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	del(lst->content);
 	free(lst);
 }
+
+/*void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	t_list *tmp;
+
+	if (!lst || !del)
+		return ;
+	tmp = lst->next->next;
+	del(&lst->next->content);
+	free(lst->next);
+	lst->next = tmp;
+}*/
