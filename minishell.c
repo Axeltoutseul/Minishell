613a927f70dcf0923b56c0caacda6112779e3670
Axel Baudrillart <axbaudri@bess-f4r5s4.clusters.42paris.fr>
2025-03-03 14:46:13 +0100

    minishell

--- minishell.c
+++ minishell.c
@@ -6,14 +6,16 @@
 /*   By: axbaudri <axbaudri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 12:43:52 by axbaudri          #+#    #+#             */
-/*   Updated: 2025/02/28 12:54:35 by axbaudri         ###   ########.fr       */
+/*   Updated: 2025/03/03 14:43:53 by axbaudri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exec_exit(void)
+void	exec_exit(t_shell *shell, t_prompt *prompt)
 {
+	free_prompt(prompt);
+	free_terminal(shell);
 	write(2, "exit\n", 5);
 	exit(1);
 }
@@ -36,7 +38,7 @@ void	execute_builtin(t_shell *shell, t_prompt *prompt)
 	else if (ft_strcmp(prompt->strs[0], "unset") == 0 && prompt->strs[1])
 		exec_unset(shell, prompt);
 	else if (ft_strcmp(prompt->strs[0], "exit") == 0 && !prompt->strs[1])
-		exec_exit();
+		exec_exit(shell, prompt);
 	else if (!existing_command(shell->splitted_path, prompt->strs[0])
 		&& ft_strcmp(prompt->strs[0], "history") != 0)
 		ft_printf("command not found: %s\n", prompt->strs[0]);
